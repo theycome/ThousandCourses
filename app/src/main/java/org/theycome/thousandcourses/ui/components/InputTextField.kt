ea375416a61661cc801129487b8978df36e8f3d6
Ivan Yakushev <theycome@mail.ru>
2025-10-14 16:25:00 +0300

    Add InputTextField

---
+++ app/src/main/java/org/theycome/thousandcourses/ui/components/InputTextField.kt
@@ -0,0 +1,58 @@
+package org.theycome.thousandcourses.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.theycome.thousandcourses.R
+import org.theycome.thousandcourses.ui.theme.ThousandCoursesTheme
+
+/**
+ * Created by Ivan Yakushev on 13.10.2025
+ */
+@Composable
+fun InputTextField(
+    value: String,
+    @StringRes placeholderId: Int,
+    modifier: Modifier = Modifier,
+) {
+    var inputText by remember { mutableStateOf(TextFieldValue(value)) }
+
+    TextField(
+        value = inputText,
+        modifier = modifier,
+        onValueChange = { inputText = it },
+        placeholder = {
+            Text(stringResource(placeholderId))
+        },
+        shape = RoundedCornerShape(30.dp),
+        colors =
+            TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            ),
+    )
+}
+
+@Composable
+@Preview
+fun InputTextFieldPreview() {
+    ThousandCoursesTheme {
+        InputTextField("", R.string.enter_caption)
+    }
+}
--- app/src/main/java/org/theycome/thousandcourses/ui/screens/GreetingScreen.kt
+++ app/src/main/java/org/theycome/thousandcourses/ui/screens/GreetingScreen.kt
@@ -2,39 +2,85 @@ package org.theycome.thousandcourses.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.theycome.thousandcourses.R
+import org.theycome.thousandcourses.ui.components.InputTextField
 
 /**
  * Created by Ivan Yakushev on 13.10.2025
  */
 @Composable
 fun GreetingScreen(modifier: Modifier = Modifier) {
+    val standardPadding = dimensionResource(R.dimen.standard_padding)
+
     Column {
-        Box(
-            modifier =
-                Modifier
-                    .padding(top = 140.dp, start = 16.dp, end = 16.dp)
-                    .height(36.dp),
-        ) {
-            Text(
-                text = stringResource(R.string.enter_caption),
-                style = MaterialTheme.typography.headlineLarge,
-            )
-        }
-//        TextField("bla", {})
+        Title(standardPadding)
+        Inputs(standardPadding)
 //        Button({}) {
 //            Text(
 //                text = "Вход",
 //            )
 //        }
 //        HorizontalDivider(Modifier.height(10.dp))
+    }
+}
+
+@Composable
+private fun Title(standardPadding: Dp) {
+    Box(
+        modifier =
+            Modifier
+                .padding(top = 140.dp, start = standardPadding, end = standardPadding)
+                .height(36.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.enter_caption),
+            style = MaterialTheme.typography.headlineLarge,
+        )
+    }
+}
+
+@Composable
+private fun Inputs(standardPadding: Dp) {
+    Column(
+        modifier =
+            Modifier
+                .padding(top = 28.dp, start = standardPadding, end = standardPadding),
+    ) {
+        Text(
+            text = stringResource(R.string.email_caption),
+            style = MaterialTheme.typography.titleMedium,
+        )
+        InputTextField(
+            value = "",
+            modifier =
+                Modifier
+                    .padding(top = 8.dp)
+                    .fillMaxWidth(),
+            placeholderId = R.string.email_placeholder,
+        )
+        Text(
+            text = stringResource(R.string.password_caption),
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.titleMedium,
+        )
+        InputTextField(
+            value = "",
+            modifier =
+                Modifier
+                    .padding(top = 8.dp)
+                    .fillMaxWidth(),
+            placeholderId = R.string.password_placeholder,
+        )
     }
 }
