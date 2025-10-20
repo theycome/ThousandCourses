53617090a0853cae235f8da55a9f07f628c7a158
Ivan Yakushev <theycome@mail.ru>
2025-10-20 16:03:43 +0300

    Add TheApplication

---
+++ app/src/main/java/org/theycome/thousandcourses/TheApplication.kt
@@ -0,0 +1,10 @@
+package org.theycome.thousandcourses
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Ivan Yakushev on 20.10.2025
+ */
+@HiltAndroidApp
+class TheApplication : Application()
