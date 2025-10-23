56004427e6cfa8b4c6bc47b732ad07523d9df20e
Ivan Yakushev <theycome@mail.ru>
2025-10-23 16:11:52 +0300

    Add StateFlowHolder

---
+++ core/src/main/kotlin/utils/StateFlowHolder.kt
@@ -0,0 +1,31 @@
+package utils
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+/**
+ * Created by Ivan Yakushev on 23.10.2025
+ */
+class StateFlowHolder<T>(
+    init: () -> T,
+) {
+    private val _flow = MutableStateFlow(init())
+    val flow = _flow.asStateFlow()
+    val value
+        get() = flow.value
+
+    fun update(transform: (T) -> T) {
+        _flow.update {
+            transform(it)
+        }
+    }
+
+    fun update(value: T) {
+        _flow.update {
+            value
+        }
+    }
+}
+
+fun <T> stateFlowHolder(init: () -> T) = StateFlowHolder(init)
