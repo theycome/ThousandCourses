7fa4989421e0ff547c773481cd8b70ef4f529307
Ivan Yakushev <theycome@mail.ru>
2025-10-23 20:19:06 +0300

    Add libraries to presentation module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -66,14 +66,41 @@ android {
 }
 
 dependencies {
+    implementation(projects.core)
+    implementation(projects.network.api)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    // compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
 
     // dagger hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+
+    // arrow
+    implementation(libs.arrow.core)
+    implementation(libs.arrow.coroutines)
+
+    // testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // kotest
+    testImplementation(libs.kotlintest)
+    testImplementation(libs.kotest.runner)
+    testImplementation(libs.kotest.assertions)
+    testImplementation(libs.kotest.framework.engine)
+    testImplementation(libs.kotest.property)
 }
