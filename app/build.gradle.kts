b6748ba55b698ae34b29af2a1ded64f3256860ea
Ivan Yakushev <theycome@mail.ru>
2025-10-24 17:10:28 +0300

    Remove compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,13 +82,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
 
-    // compose
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-
     // retrofit
     implementation(libs.retrofit)
 
